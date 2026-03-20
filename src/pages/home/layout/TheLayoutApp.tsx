1874c30c49ae46404e0515724e595c2f702dec15
nhatnguyen150100 <nhatnguyen150100@gmail.com>
2026-03-20 20:41:48 +0700

    refactor: update BottomBar positioning and adjust layout padding in TheLayoutApp

--- src/pages/home/layout/TheLayoutApp.tsx
+++ src/pages/home/layout/TheLayoutApp.tsx
@@ -6,7 +6,7 @@ export default function TheLayoutApp() {
   return (
     <div className="overflow-hidden sm:w-full sm:flex sm:justify-center sm:items-center">
       <div className="h-screen w-screen flex flex-col justify-between items-start bg-gray-100 md:max-w-[450px] overflow-hidden">
-        <div className="h-full w-full overflow-y-auto sm:overflow-x-hidden">
+        <div className="h-full w-full overflow-y-auto sm:overflow-x-hidden pb-16">
           <Outlet />
         </div>
         <BottomBar />
